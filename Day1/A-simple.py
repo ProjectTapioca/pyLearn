17a67f49ea59de0cd8f5ce55ee51cddced729ecc
ProjectLuke <luketrueno86@gmail.com>
2021-11-11 19:44:06 -0800

    Added some notes for Day1

---
+++ Day1/A-simple.py
@@ -0,0 +1,22 @@
+#Write your code below this line 👇
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+#Print on a new line with \n also works
+
+print("A\nB\nC")
+
+#Can also concatenate with + and include spaces by using " "
+print("Hello" + " " + "World")
+
+#There are a few ways to do input funciton
+input("What is your name?")
+
+#or
+
+#input() will get user input in the console
+#Then print() will print the word "Hello" and the uesr input
+print("Hello " + input("What is your name?"))
+
+#Day1 Challenge is in replit
