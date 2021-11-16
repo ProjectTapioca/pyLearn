bd4ddef3f527e957f3a81c5c41f1c525c32a1e6e
ProjectLuke <luketrueno86@gmail.com>
2021-11-15 17:18:29 -0800

    Math Operations

---
+++ Day2/MathOps.py
@@ -0,0 +1,20 @@
+#Order of Operations
+# ()
+#  **
+#  */
+#  =-
+# LR - left to right on whichever hs priority
+
+# // Floor Division
+
+print(3 * 3 + 3 / 3 - 7)
+
+#Rounding
+print(round(8 / 3, 2))
+
+score = 0
+height = 1.8
+isWinning = True
+
+#fstring
+print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
