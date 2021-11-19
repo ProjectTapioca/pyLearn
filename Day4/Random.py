c07be8a89299affe8c6b23a07ab9a9ad23da7e85
ProjectLuke <luketrueno86@gmail.com>
2021-11-18 17:15:35 -0800

    Add lesson on random

---
+++ Day4/Random.py
@@ -0,0 +1,9 @@
+import random
+#import my_module 
+#^^That is how you create/import your own module
+
+random_integer = random.randint(1,10)
+print(random_integer)
+
+#The my_module refers to the module and the pi part refers to the function in the module
+#print(my_module.pi)
