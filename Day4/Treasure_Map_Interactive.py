0c42e148d29af2bb679093132294d066cb5aa957
ProjectLuke <luketrueno86@gmail.com>
2021-11-23 15:51:39 -0800

    Add Treasure chest location interactive

--- Day4/Treasure_Map_Interactive.py
+++ Day4/Treasure_Map_Interactive.py
@@ -6,13 +6,16 @@ map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 # 🚨 Don't change the code above 👆
-
+#Can just replace the box with an x through the input provided
 #Write your code below this row 👇
+list_position=list(position)
+x = int(list_position[1])
+y = int(list_position[0])
 
+list_position = [int(i) for i in list_position]
+#test_list = [int(i) for i in test_list]
 
-
-
-
+map[x-1][y-1] = "X"
 
 #Write your code above this row 👆
 
