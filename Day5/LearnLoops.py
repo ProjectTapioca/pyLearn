04f9b520f814b13d3fe27eba495b173e8be8999c
ProjectLuke <luketrueno86@gmail.com>
2021-11-24 10:59:17 -0800

    Add loop lesson

---
+++ Day5/LearnLoops.py
@@ -0,0 +1,8 @@
+fruits = ["Apple", "Peach", "Pear"]
+
+#fruit is the variable assigned to the items in the list 'fruits'
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
+print(fruits)
+
