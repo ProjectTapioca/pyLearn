6d323783046168b82cab8386a9404c2e6980e876
lugalino <ugalinoluke@gmail.com>
2021-12-02 15:37:19 -0800

    Add Learning Lesson

--- Day5/LearnLoops.py
+++ Day5/LearnLoops.py
@@ -6,3 +6,13 @@ for fruit in fruits:
     print(fruit + " Pie")
 print(fruits)
 
+#Loops and range
+for number in range(1,10,2):
+    print(number)
+#This only prints 1 - 9, have to change the 10 to a 11 if want to print from 1 - 10
+#The 2 shows how it will skip/step acorss different numbers
+
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
