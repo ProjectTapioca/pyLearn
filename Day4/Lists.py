c4f800c88e0894d53dc0e62f0fec8354a0103102
lugalino <ugalinoluke@gmail.com>
2021-12-10 13:26:39 -0800

    Add an extra note to lists

--- Day4/Lists.py
+++ Day4/Lists.py
@@ -6,3 +6,5 @@ fruit = ["apple", "banana", "pear"]
 #Can add to the list by doing
 #fruit.append("coconut")
 
+#Join all the elements in the list and turn it into a String.
+    # print(f"{' '.join(display)}")
