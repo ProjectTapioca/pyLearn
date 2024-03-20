d3e3918ca321c733747e37570e965ce7d84929cf
Project Tapioca <39897299+ProjectTapioca@users.noreply.github.com>
2024-03-19 18:28:51 -0700

    Simple exercise to understand functions better

---
+++ Day8/Exercises.py
@@ -0,0 +1,10 @@
+#Create a function called greet()
+#Write 3 print statements inside the function
+#Call the greet() function and run your code
+
+def greet():
+    print("This is the first line")
+    print("This is out to be the second")
+    print("Finally the last")
+
+greet()
